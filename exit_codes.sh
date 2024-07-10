7fca091f034b175f2eece2a920dcd5a385301472
Nithin S <119758355+Nithin-s-gowda@users.noreply.github.com>
2024-07-10 23:07:25 +0530

    Update exit_codes.sh

--- exit_codes.sh
+++ exit_codes.sh
@@ -2,5 +2,5 @@
 # Experimenting with status codes 
 ls -l > /dev/null 
 echo "The status code of the ls command was: $?" 
-lzl 2> /dev/null 
+lzl 2 > /dev/null 
 echo "The status code of the non-existing lzl command was: $?"

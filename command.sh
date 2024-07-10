0f3f4c19ba3605f082acf86f7c4e1751f4a69f6d
Nithin S <119758355+Nithin-s-gowda@users.noreply.github.com>
2024-07-10 10:24:34 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "Hello, world!"
+echo "1.pwd - present working directory"
+echo "2.ls - list"
+echo "3.ls -l -list content in long format"
+echo "4.ls -a - list all teh hidden files"
+echo "5.cat - new file"
 ls -l
